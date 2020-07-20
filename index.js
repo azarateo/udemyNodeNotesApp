eda393a1f178f21997c4395b2a900beac9e08201
Alejandro Zarate <azarateo@gmail.com>
2020-07-20 14:00:38 -0500

    Section 3 - Lesson 9 Importing Node.js Core Modules

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
-const checkUtils = require('./src/utils.js')
 
-checkUtils()
+//const checkUtils = require('./src/utils.js')
+//checkUtils()
 
 //fs.writeFileSync('notes.txt','Hello world')
 //console.log('File writen')
+
+//Lesson 9 Importing Node.js Core Modules
+fs.appendFileSync('notes.txt','\nMy name is Alejandro')
