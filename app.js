c72d7b85d28d3f925966d3e5e41c2936fb022a94
Alejandro Zarate <azarateo@gmail.com>
2020-07-21 11:48:02 -0500

    Section 3 Lesson 11

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const validator = require('validator')
 const notes = require('./notes.js')
-const thenotes = notes()
-console.log(thenotes)
+
+
+console.log(validator.isEmail('azarateo@gmail.com'))
+console.log(validator.isEmail('gmail.com'))
+console.log(validator.isURL('https://www.google.com'))
