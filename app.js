afae869ae41db9d50a408afcd88a324586e9ca87
Alejandro Zarate <azarateo@gmail.com>
2020-07-23 09:33:22 -0500

    Section 4 lesson 16 challenge

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ yargs.command({
         console.log('Adding a note')
     }
 })
+yargs.command({
+    command:'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note')
+    }
+})
+yargs.command({
+    command:'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
+yargs.command(
+    {
+       command: 'list nodes',
+       describe: 'List all nodes',
+       handler: function(){
+           console.log('Listing all nodes in the repository')
+       }
+    }
+)
+console.log(yargs.argv)
